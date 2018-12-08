a3a5e4e8a4f5d6bae694ed3a8e49d6ba9840b61f
jofrisch <jfrisch@ulb.ac.be>
2018-12-09 00:03:25 +0100

    read and store data in 'data' folder.

--- adhocs.py
+++ adhocs.py
@@ -1,8 +1,14 @@
 import pandas as pd
 import numpy as np
 
+import os
+
 from sklearn.base import BaseEstimator, ClassifierMixin, TransformerMixin
 
+
+###########################
+##### Feature creation ####
+###########################
 
 class AdHocFeaturesCreation(BaseEstimator, TransformerMixin):
     """ Create features that are specific to the problem's dataset."""
@@ -26,7 +32,7 @@ class AdHocFeaturesCreation(BaseEstimator, TransformerMixin):
 ##############################
 
 class CheckDFQuality(BaseEstimator, TransformerMixin):
-  
+  """ Performs adhoc checks on a dataframe, within a pipeline"""
   def __init__(self, position):
     self.position = position
   
@@ -34,15 +40,15 @@ class CheckDFQuality(BaseEstimator, TransformerMixin):
     return self
   
   def transform(self, X, y=None):
-    print("####################")
+    print("#"*20)
     print("Data Quality for ", self.position)
-    print("####################")
+    print("#"*20)
     print("DataFrame/Series contains any nulls:", X['native-country'].isnull().any())
     return X
 
 
 class CheckNpArrayQuality(BaseEstimator, TransformerMixin):
-  
+  """ Performs adhoc checks on a numpy array, within a pipeline"""  
   def __init__(self, position):
     self.position = position
   
@@ -50,9 +56,9 @@ class CheckNpArrayQuality(BaseEstimator, TransformerMixin):
     return self
   
   def transform(self, X, y=None):
-    print("####################")
+    print("#"*20)
     print("Data Quality for ", self.position)
-    print("####################")
+    print("#"*20)
     print('Contains nulls:')
     print(np.any(np.isnan(X)))
     print("Is finite:")
@@ -63,12 +69,53 @@ class CheckNpArrayQuality(BaseEstimator, TransformerMixin):
     print(X.shape)
     print('Nb of unique lines:')
     print(len(np.unique(X, axis=0)))
-    print("####################")
+    print("#"*20)
     print("Print lines with nulls:")
     print(X[np.isnan(X).any(axis=1)][:3])
-    print("####################")
+    print("#"*20)
     print('First 5 lines:')
     print(X[:5])
     return X
 
 
+#########################
+#### Download data ######
+#########################
+
+
+def download_data(subset='train'):
+
+    fname = os.path.join(os.getcwd(), "data", "adult_"+ subset + ".csv")
+
+    if not os.path.isfile(fname) :
+        # Construct the data URL.
+        if subset == 'train':
+            csv_url = 'http://mlr.cs.umass.edu/ml/machine-learning-databases/adult/adult.data'
+        else:
+            csv_url = 'http://mlr.cs.umass.edu/ml/machine-learning-databases/adult/adult.test'
+        # Define the column names.
+        names = [
+            'age', 'workclass', 'fnlwgt', 'education', 'education-num',
+            'marital-status', 'occupation', 'relationship', 'race', 'sex',
+            'capital-gain', 'capital-loss', 'hours-per-week', 'native-country',
+            'earns_over_50K']
+        # Read the CSV.
+        print('Downloading {subset} dataset to __data__/ ...')
+        df = pd.read_csv(
+            csv_url,
+            sep=', ',
+            names=names,
+            skiprows=int(subset == 'test'),
+            na_values='?')
+
+        # Split into feature matrix X and labels y.
+        df.earns_over_50K = df.earns_over_50K.str.contains('>').astype(int)
+        df.to_csv(fname, index=False)
+        print("Data has been downloaded, and copied to local disk")
+
+    else:
+        df = pd.read_csv(fname)
+        print("Data has been copied from local disk")
+
+    X, y = df.drop(['earns_over_50K'], axis=1), df.earns_over_50K
+    return X, y
--- main.py
+++ main.py
@@ -1,52 +1,13 @@
 import numpy as np
 import pandas as pd
 import re
-from sklearn.model_selection import GridSearchCV, train_test_split
+
 from pipelines import get_pipeline
+from adhocs import download_data
 from sklearn.metrics import roc_auc_score
-import sys
-import os.path
+
 
 rs = 123
-
-
-def download_data(subset='train'):
-
-    fname = "adult_"+ subset + ".csv"
-
-    if not os.path.isfile(fname) :
-        # Construct the data URL.
-        if subset == 'train':
-            csv_url = 'http://mlr.cs.umass.edu/ml/machine-learning-databases/adult/adult.data'
-        else:
-            csv_url = 'http://mlr.cs.umass.edu/ml/machine-learning-databases/adult/adult.test'
-        # Define the column names.
-        names = [
-            'age', 'workclass', 'fnlwgt', 'education', 'education-num',
-            'marital-status', 'occupation', 'relationship', 'race', 'sex',
-            'capital-gain', 'capital-loss', 'hours-per-week', 'native-country',
-            'earns_over_50K']
-        # Read the CSV.
-        print('Downloading {subset} dataset to __data__/ ...')
-        df = pd.read_csv(
-            csv_url,
-            sep=', ',
-            names=names,
-            skiprows=int(subset == 'test'),
-            na_values='?')
-
-        # Split into feature matrix X and labels y.
-        df.earns_over_50K = df.earns_over_50K.str.contains('>').astype(int)
-        df.to_csv(fname, index=False)
-        print("Data has been downloaded, and copied to local disk")
-
-    else:
-        df = pd.read_csv(fname)
-        print("Data has been copied from local disk")
-
-    X, y = df.drop(['earns_over_50K'], axis=1), df.earns_over_50K
-    return X, y
-
 
 
 if __name__ == '__main__':
